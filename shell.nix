4e6756284aaabf875eb7d006b084f61a2c122578
tusing <tushar@singal.io>
2022-02-06 01:18:03 -0800

    improve env var setting

--- shell.nix
+++ shell.nix
@@ -7,8 +7,7 @@ pkgs.mkShell {
     yubikey-manager
     yubico-piv-tool
   ];
-  shellHook = ''
-    export SO_PATH="${pkgs.libp11}/lib/engines/pkcs11.dylib"
-    export MODULE_PATH="${pkgs.opensc}/lib/opensc-pkcs11.so"
-  '';
+
+  SO_PATH = "${pkgs.libp11}/lib/engines/pkcs11.dylib";
+  MODULE_PATH = "${pkgs.opensc}/lib/opensc-pkcs11.so";
 }
